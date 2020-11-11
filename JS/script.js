fc57bfadda89cd9ac731da84a84a35eb6a4c3ad7
Adrian Albert Ciubucciu <ciubucciu.adrianalbert@gmail.com>
2020-11-11 11:37:56 +0200

    finalize the functionality

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,25 @@
 //create array that contains all the images on the page
 const IMAGES = document.querySelectorAll("img");
 
+const SIZES = {
+
+    showcase : "100vw",
+    reason : "(max-width: 799px) 100vw, 372px",
+    feature : "(max-width: 799px) 100vw, 558px",
+    story : "(max-width: 799px) 100vw, 670px"
+
+}
+
 function makeSrcset(imgSrc) {
 
     let markup = [];
+    
     let width = 400; // the smallest
 
     for (let i = 0; i < 5; i ++) { // we generate 5 different sizes
 
         markup[i] = imgSrc + "-" + width + ".jpg " + width + "w";
+
         width+=400;
 
     }
@@ -20,12 +31,18 @@ function makeSrcset(imgSrc) {
 for (let i = 0; i < IMAGES.length; i++) {
 
     let imgSource = IMAGES[i].getAttribute("SRC");
+
     // "-800.jpg" has 8 characters, tht's why we slice 8
     imgSource = imgSource.slice(0, -8);
+
     let srcset = makeSrcset(imgSource);
-    console.log(srcset);
+
+    IMAGES[i].setAttribute("srcset", srcset);
 
     let type = IMAGES[i].getAttribute("data-type");
-    console.log(type);
+
+    let sizes = SIZES[type];
+
+    IMAGES[i].setAttribute("sizes", sizes);
 
 }
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Automated_Responsive_Images_Markup
 JavaScript, HTLM and CSS Project.
+
+We will use JS to inject the responsive images markup when it's appropiate into the website. 
+
+Touched subjects : 
+- loops within loops
+- objects
+- attributes
+
